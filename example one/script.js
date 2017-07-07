14d33c46298d1b20169da7a976604dab9e7adc18
vetaluga <vrtalugavetaluga@gmail.com>
2017-07-07 17:55:18 +0300

    file

---
+++ example one/script.js
@@ -0,0 +1,30 @@
+var btn_prev = document.querySelector('#gallery .buttons .prev');
+var btn_next = document.querySelector('#gallery .buttons .next');
+
+var images = document.querySelectorAll('#gallery .photos img');
+var i = 0;
+
+var number = document.getElementById('out');
+
+
+
+btn_prev.onclick = function () {
+  images[i].className = '';
+  i--;
+
+  if (i < 0) {
+    i = images.length - 1;
+  }
+  images[i].className = 'showed';
+  number.innerHTML = i;
+}
+
+btn_next.onclick = function() {
+  images[i].className = '';
+  i++;
+  if (i >= images.length) {
+    i = 0;
+  }
+  images[i].className = 'showed';
+  number.innerHTML = i;
+}
