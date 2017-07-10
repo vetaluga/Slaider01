210904d7da750fea223e9578f054614aae90137b
vetaluga <vrtalugavetaluga@gmail.com>
2017-07-10 21:42:26 +0300

    Did decide issue

--- script.js
+++ script.js
@@ -3,30 +3,52 @@ var btn_next = document.querySelector('#gallery .buttons .next');
 
 var images = document.querySelectorAll('#gallery .photos img');
 var i = 0;
-
+images[0] = null;
 var number = document.getElementById('out');
+
+console.log(images);
+function EnteredNumber(event){
+  images[i].className = '';
+  if( event.key == 1 ){
+    i = 0;
+  } else if ( event.key == 2 ){
+    i = 1;
+  } else if ( event.key == 3 ){
+    i = 2;
+  } else if ( event.key == 4 ){
+    i = 3;
+  } else if ( event.key == 5 ){
+    i = 4;
+  } else if ( event.key == 6 ){
+    i = 5;
+  } else if ( event.key == 7 ){
+    i = 6;
+  } else if ( event.key == 8 ){
+    i = 7;
+  } else if ( event.key == 9 ){
+    i = 8;
+  }
+  images[i].className = 'showed';
+  number.innerHTML = i + 1 + " out of 9";
+}
 
 btn_prev.onclick = function () {
   images[i].className = '';
-  i = i - 3;
+  i--;
 
-  if (i == -1) {
+  if (i < 0) {
     i = images.length - 1;
   }
   images[i].className = 'showed';
-  number.innerHTML = i;
+  number.innerHTML = i + 1 + " out of 9";
 }
 
 btn_next.onclick = function() {
   images[i].className = '';
-  i = i + 3;
-  if (i == 10) {
+  i++;
+  if ( i >= images.length ){
     i = 0;
-  } else if ( i == 11){
-    i = 1;
-  } else if ( i == 12) {
-    i = 2;
   }
   images[i].className = 'showed';
-  number.innerHTML = i;
+  number.innerHTML = i + 1 + " out of 9";
 }
